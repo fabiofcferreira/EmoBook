eb9571c0d7ad094c415364d74b98cf3ebe26dc16
Henrique Dias <hacdias@gmail.com>
2016-03-29 22:40:58 +0100

    emoticons on whats on your mind

--- extension/js/content.js
+++ extension/js/content.js
@@ -30,10 +30,37 @@ function addCommentsIcon() {
 }
 
 function addWhatsOnYourMindIcon() {
+  if (document.getElementsByClassName('emobook-composer').length > 0) {
+    return;
+  }
 
+  var composer = document.getElementById('pagelet_composer');
+  var icons = composer.getElementsByClassName('_1dsp')[0];
+  var iframe = document.createElement('iframe');
+  iframe.setAttribute('src', popup);
+  iframe.setAttribute('width', '100%');
+  iframe.setAttribute('height', 250);
+
+  var div = document.createElement('div');
+  div.setAttribute('class', 'emobook-composer closed');
+  div.appendChild(iframe);
+
+  var list = composer.getElementsByClassName('_1dso')[0];
+  var li = document.createElement('li');
+  li.setAttribute('class', 'emobook-composer-button');
+  li.innerHTML = `<div class="_42ft _4jy0 _55pi _5vto _55_p _2agf _p _4jy3 _517h _51sy">Emoticons</div>`;
+  li.addEventListener('click', function(event) {
+    event.preventDefault();
+    li.classList.toggle('openToggler');
+    div.classList.toggle('opened');
+  });
+
+  list.insertBefore(li, list.childNodes[0]);
+  icons.appendChild(div);
 }
 
 function addIconLoop() {
+  addWhatsOnYourMindIcon();
   addChatIcon();
   addCommentsIcon();
   setTimeout(addIconLoop, 100);
@@ -54,7 +81,6 @@ function generateElement() {
 
 document.addEventListener('DOMContentLoaded', () => {
   generateElement();
-  addWhatsOnYourMindIcon();
   addIconLoop();
 });
 
